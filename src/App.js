58b656a3c919b0349e619ad5301fc6f91538a34a
danielmkwolek <daniel.m.kwolek@gmail.com>
2022-06-01 13:46:31 -0700

    implement post request and get request features and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
 import {Routes, Route, Link, Navigate} from "react-router-dom";
+import {useState} from "react";
 import VinylsView from './Components/VinylsView';
 
 function App() {
+
+  const [artistName, setArtistName] = useState("")
+  const [albumName, setAlbumName] = useState("")
+
+  const artistHandleOnChange = (event) => {
+    setArtistName(event.target.value)
+  }
+
+  const albumHandleOnChange = (event) => {
+    setAlbumName(event.target.value)
+  }
+
+  const handleOnSubmit= (event) => {
+    event.preventDefault()
+
+    fetch("http://127.0.0.1:3000/vinyls/", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        artistName: artistName,
+        albumName: albumName
+      })
+    })
+      .then(response => response.json())
+      .then(vinylObj => console.log(vinylObj))
+      .catch(e => console.log(e));
+  }
+  
   return (
     <div className="App">
-
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" placeholder="artistname" value={artistName} onChange={artistHandleOnChange}/>
+        <input type="text" placeholder="albumname" value={albumName} onChange={albumHandleOnChange}/>
+        <input type="submit" value="submit"/>
+      </form>
       <main>
         <Routes>
           <Route path="/" element={<VinylsView />} />
--- src/Components/VinylsView.js
+++ src/Components/VinylsView.js
@@ -4,16 +4,16 @@ import { useEffect, useState } from "react";
 const VinylsView = () => {
     const [vinylData, setVinylData] = useState([]);
 
-    const vinyls = vinylData.map((value) => {
-        return <Vinyl data={value} key={value._id}/>
-    })
-
     useEffect(() => {
         fetch("http://127.0.0.1:3000/vinyls")
             .then(response => response.json())
             .then(data => setVinylData(data.vinyls))
             .catch(e => console.log(e))
     }, [])
+
+    const vinyls = vinylData.map((value) => {
+        return <Vinyl data={value} key={value._id}/>
+    })
 
     return (
         <div>
